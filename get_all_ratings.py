304d52ac18dda994978f8e72e2b385f547aff0a1
Noah Adhikari <noahadhikari@berkeley.edu>
2023-04-03 14:05:27 -0700

    update ratings file

--- get_all_ratings.py
+++ get_all_ratings.py
@@ -15,7 +15,8 @@ class Rating:
     binVoxId: int
 
 
-URL = "https://model-rating.vercel.app/api/trpc/rating.getAllRatings?batch=1&input={%220%22%3A{%22json%22%3Anull%2C%22meta%22%3A{%22values%22%3A[%22undefined%22]}}}"
+# Replace A1 at the end with A<PAGE NUMBER> to get the next page of ratings
+URL = "https://model-rating.vercel.app/api/trpc/rating.getRatingsWithPage?batch=1&input={%220%22%3A{%22json%22%3A{%22page%22%3A1}}}"
 
 
 def getRatings() -> List[Rating]:
@@ -24,54 +25,14 @@ def getRatings() -> List[Rating]:
     items = data[0]['result']['data']['json']
 
     return items
-
-
-def load_google_api_key():
-    #load api key from .env
-    with open(".env", "r") as f:
-        for line in f.readlines():
-            if line.startswith("GOOGLE_API_KEY"):
-                return line.split("=")[1].strip()
     
-def download_binvox_from_rating(rating):
-    """ Given a rating, download its binvox file from Google Drive.
-        Only downloads if the binvoxId is not None.
-    """
     
-    BASE_URL = "https://www.googleapis.com/drive/v3/files/"
-    API_KEY = load_google_api_key()
-    
-    if rating['binVoxId'] is None:
-        return
-    
-    url = f"{BASE_URL}{rating['binVoxId']}?alt=media&key={API_KEY}"
-    
-    download_and_rename(url, f"data/binvox/{rating['folderId']}", f"{rating['modelId']}.binvox")
-
-def download_stl_from_rating(rating):
-    BASE_URL = "https://www.googleapis.com/drive/v3/files/"
-    API_KEY = load_google_api_key()
-
-    if rating['stlId'] is None:
-        return
-    
-    url = f"{BASE_URL}{rating['stlId']}?alt=media&key={API_KEY}"
-    
-    download_and_rename(url, f"data/stl/{rating['folderId']}", f"{rating['modelId']}.stl")
-
-def download_and_rename(url, root_dir, filename):
-    if (not os.path.exists(root_dir)):
-        os.makedirs(root_dir)
-        
-    r = requests.get(url, allow_redirects=True)
-    with open(os.path.join(root_dir, filename), 'wb') as f:
-        f.write(r.content)
-    
-if __name__ == "__main__":
+def main():
     ratings = getRatings()
-    # for rating in ratings:
-    #     download_binvox_from_rating(rating)
-    #     download_stl_from_rating(rating)
     with open("data/ratings.json", "w") as f:
         f.write(json.dumps(ratings, indent=4))
+        
+        
+if __name__ == "__main__":
+    main()
     
